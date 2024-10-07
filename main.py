43c21c0b423c0c1ac9c6c20430910c360af960c3
melinayu <melinayusafitri@gmail.com>
2024-10-08 00:04:39 +0700

    Add Validation Stock

--- main.py
+++ main.py
@@ -65,3 +65,72 @@ elif Amount_of_Money == Payment:
 else: 
     Return_of_Money = Amount_of_Money - Payment    # Amount_of_Money > Payment
     print(f"Uang Kembali Anda: {Return_of_Money}")
+
+
+#========EXERCISE 3 MARKET PROJECT========#
+
+# Create the Price List of Fruit Purchases
+Price_Apple = 10000
+Price_Orange = 15000
+Price_Grape = 20000
+
+# Stock of Fruit Purchases
+Stock_Amount_Apple = 5
+Stock_Amount_Orange = 7
+Stock_Amount_Grape = 6
+
+# Condition 1 - "When there are too many apples"
+Amount_Apple = int(input("Masukkan Jumlah Apel: "))
+while Amount_Apple > Stock_Amount_Apple:
+    print("Jumlah yang dimasukkan terlalu banyak")
+    print("Stok Apel tinggal : 5")
+    Amount_Apple = int(input("Masukkan Jumlah Apel: "))
+print()
+
+# Condition 2 - "When there are too many oranges"
+Amount_Orange = int(input("Masukkan Jumlah Jeruk: "))
+while Amount_Orange > Stock_Amount_Orange:
+    print("Jumlah yang dimasukkan terlalu banyak")
+    print("Stok Jeruk tinggal : 7")
+    Amount_Orange = int(input("Masukkan Jumlah Jeruk: "))
+print()
+
+# Condition 3 - "When there are too many grapes"
+Amount_Grape = int(input("Masukkan Jumlah Anggur: "))
+while Amount_Grape > Stock_Amount_Grape:
+    print("Jumlah yang dimasukkan terlalu banyak")
+    print("Stok Anggur tinggal : 6")
+    Amount_Grape = int(input("Masukkan Jumlah Anggur: "))
+print()
+
+# Calculate the Price List of Fruit Purchases
+Price_Apple_Total = Price_Apple * Amount_Apple
+Price_Orange_Total = Price_Orange * Amount_Orange
+Price_Grape_Total = Price_Grape * Amount_Grape
+
+# Show the Shopping Detail of Fruit Purchases
+print("Detail Belanja")
+print(f"Apel: {Amount_Apple} x {Price_Apple} = {Price_Apple_Total}")
+print(f"Jeruk: {Amount_Orange} x {Price_Orange} = {Price_Orange_Total}")
+print(f"Anggur: {Amount_Grape} x {Price_Grape} = {Price_Grape_Total}")
+print()
+print("Total price: ", Price_Apple_Total + Price_Orange_Total + Price_Grape_Total)
+print()
+
+# Show the Payment of Fruit Purchase
+Payment = 275000
+Amount_of_Money = input("Masukkan Jumlah Uang: ")
+Amount_of_Money = int(Amount_of_Money)
+print()
+
+# Condition - "When the amount of money given is less"
+
+while Amount_of_Money < Payment:
+    Lack_of_Money = Payment - Amount_of_Money
+    print(f"Uang Anda Kurang Sebesar: {Lack_of_Money}")
+    Amount_of_Money2 = int(input("Masukkan Jumlah Uang: "))
+    if Amount_of_Money2 >= Payment:
+        print("Terima kasih!")
+        Return_of_Money = Amount_of_Money2 - Payment
+        print(f"Uang Kembali Anda: {Return_of_Money}")
+        break
